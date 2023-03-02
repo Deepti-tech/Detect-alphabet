eabc7ffaf7f670ff0411dee5bdb9481c3f611611
deepti <singhdeepti311@gmail.com>
2023-03-02 23:14:19 +0530

    front camera disabled

--- script.js
+++ script.js
@@ -9,7 +9,6 @@
         // Load the image model and setup the webcam
         async function init() {
             document.getElementById("Start").style.display = "none";
-            document.getElementById("Rev").style.display = "flex";
             document.getElementById("Pause").style.display = "flex";
             const modelURL = URL + "model.json";
             const metadataURL = URL + "metadata.json";
@@ -47,34 +46,34 @@
             webcam.play()
         }
 
-        async function reverse() {
-            // document.getElementById("webcam-container").removeChild(webcam.canvas);
-            const modelURL = URL + "model.json";
-            const metadataURL = URL + "metadata.json";
-            const devices = await navigator.mediaDevices.enumerateDevices()
-            // console.log(devices)
-            // load the model and metadata
-            // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-            // or files from your local hard drive
-            // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-            model = await tmImage.load(modelURL, metadataURL);
-            maxPredictions = model.getTotalClasses();
+        // async function reverse() {
+        //     // document.getElementById("webcam-container").removeChild(webcam.canvas);
+        //     const modelURL = URL + "model.json";
+        //     const metadataURL = URL + "metadata.json";
+        //     const devices = await navigator.mediaDevices.enumerateDevices()
+        //     // console.log(devices)
+        //     // load the model and metadata
+        //     // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+        //     // or files from your local hard drive
+        //     // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+        //     model = await tmImage.load(modelURL, metadataURL);
+        //     maxPredictions = model.getTotalClasses();
             
-            // Convenience function to setup a webcam
-            const flip = false; // whether to flip the webcam
-            webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
-            // await webcam.setup({ facingMode: "environment" });  // request access to the webcam
-            await webcam.setup({ deviceId: devices[0].deviceId })
-            await webcam.play();
-            window.requestAnimationFrame(loop);
+        //     // Convenience function to setup a webcam
+        //     const flip = false; // whether to flip the webcam
+        //     webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+        //     // await webcam.setup({ facingMode: "environment" });  // request access to the webcam
+        //     await webcam.setup({ deviceId: devices[0].deviceId })
+        //     await webcam.play();
+        //     window.requestAnimationFrame(loop);
 
-            // append elements to the DOM
-            document.getElementById("webcam-container").appendChild(webcam.canvas);
-            labelContainer = document.getElementById("label-container");
-            for (let i = 0; i < maxPredictions; i++) { // and class labels
-                labelContainer.appendChild(document.createElement("div"));
-            }
-        }
+        //     // append elements to the DOM
+        //     document.getElementById("webcam-container").appendChild(webcam.canvas);
+        //     labelContainer = document.getElementById("label-container");
+        //     for (let i = 0; i < maxPredictions; i++) { // and class labels
+        //         labelContainer.appendChild(document.createElement("div"));
+        //     }
+        // }
 
         async function loop() {
             webcam.update(); // update the webcam frame
