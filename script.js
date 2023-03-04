30a60068b36dd7c3797a36fd8f81396b454e4833
deepti <singhdeepti311@gmail.com>
2023-03-04 09:53:25 +0530

    camera funtion added

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
 // More API functions here:
         // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
-
+        
         // the link to your model provided by Teachable Machine export panel
         const URL = "https://teachablemachine.withgoogle.com/models/Kuqt40xlW/";
 
         let model, webcam, labelContainer, maxPredictions;
+        
+        
 
         // Load the image model and setup the webcam
         async function init() {
             document.getElementById("Start").style.display = "none";
             document.getElementById("Pause").style.display = "flex";
-            const modelURL = URL + "model.json";
-            const metadataURL = URL + "metadata.json";
-            const devices = await navigator.mediaDevices.enumerateDevices()
+            // const modelURL = URL + "model.json";
+            // const metadataURL = URL + "metadata.json";
+            // const devices = await navigator.mediaDevices.enumerateDevices()
             // load the model and metadata
             // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
             // or files from your local hard drive
             // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-            model = await tmImage.load(modelURL, metadataURL);
+            // model = await tmImage.load(modelURL, metadataURL);
             maxPredictions = model.getTotalClasses();
 
             // Convenience function to setup a webcam
-            const flip = true; // whether to flip the webcam
-            webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
-            await webcam.setup({ facingMode: "environment" });  // request access to the webcam
-            // await webcam.setup({ deviceId: devices[0].deviceId })
-            await webcam.play();
-            window.requestAnimationFrame(loop);
-
-            // append elements to the DOM
-            document.getElementById("webcam-container").appendChild(webcam.canvas);
+            
             labelContainer = document.getElementById("label-container");
             for (let i = 0; i < maxPredictions; i++) { // and class labels
                 labelContainer.appendChild(document.createElement("div"));
@@ -91,3 +85,23 @@
                 labelContainer.childNodes[i].innerHTML = classPrediction;
             }
         }
+
+        async function camera(){
+            const modelURL = URL + "model.json";
+            const metadataURL = URL + "metadata.json";
+            model = await tmImage.load(modelURL, metadataURL);
+            const flip = true; // whether to flip the webcam
+            webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+            await webcam.setup({ facingMode: "environment" });  // request access to the webcam
+            // await webcam.setup({ deviceId: devices[0].deviceId })
+            await webcam.play();
+            window.requestAnimationFrame(loop);
+
+            // append elements to the DOM
+            document.getElementById("webcam-container").appendChild(webcam.canvas);
+            
+        }
+        
+        window.onload = function() {
+            camera();
+          };
